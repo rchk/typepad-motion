b2d2bc3f7c4890c428d421075799ac510474a67f
Mark Paschal <mark@sixapart.com>
2009-08-07 19:09:15 +0000

    Bump versions as we shipped a release already

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name='motion',
-    version='1.0',
+    version='1.1',
     description='Community microblogging in the TypePad cloud',
     author='Six Apart',
     author_email='python@sixapart.com',
